c1cf6e58bdffb5c6bdb0a2e33263cf09a3bc3c8b
i4inga4xyk <i4inga4xyk@gmail.com>
2024-04-21 13:18:23 +0300

    added validation

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,9 +1,10 @@
-import { IsEmail, MinLength } from "class-validator";
+import { IsEmail, IsNotEmpty, MinLength } from "class-validator";
 
 export class CreateUserDto {
     @IsEmail()
     email: string;
 
+    @IsNotEmpty()
     username: string;
 
     @MinLength(6, {'message': 'Password must be more than 6 characters.'})
